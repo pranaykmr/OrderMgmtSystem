8ca65b73000910d1f8af46c783dff209f48bda07
Pranay Kumar <pranay.kmr@live.com>
2019-05-24 20:37:06 +0530

    New Models

---
+++ src/app/models/ShippedBy.ts
@@ -0,0 +1,10 @@
+export class ShippedBy{
+    constructor(){
+        this.ShipperId = "";
+        this.ShipperName = "";
+        this.ShippingMode_Id = "";
+    }
+    public ShipperId : string;
+    public ShipperName : string;
+    public ShippingMode_Id : string;   
+}
---
+++ src/app/models/ShippingDetails.ts
@@ -0,0 +1,20 @@
+export class ShippingDetails{
+    constructor(){
+        this.Order_No = "";
+        this.PO_No = "";
+        this.Invoice_No = "";        
+        this.Shipping_Date = null;
+        this.Weight = 0;        
+        this.ShippedById = "";
+        this.ShippingModeId = "";
+        this.Advised = null;
+    }
+    public Order_No : string;
+    public PO_No : string;
+    public Invoice_No : string; 
+    public Shipping_Date : Date;
+    public Weight : number;
+    public ShippedById : string;   
+    public ShippingModeId : string;
+    public Advised : Date;
+}
